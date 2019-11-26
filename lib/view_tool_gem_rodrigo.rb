93951233b493bef92d925261a970fcbadc55f63e
Rodrigo Castro <rodrigocastro@claretiano.edu.br>
2019-11-26 13:00:00 -0300

    Dev tool primeiro commit

---
+++ lib/view_tool_gem_rodrigo.rb
@@ -0,0 +1,6 @@
+require "view_tool_gem_rodrigo/version"
+
+module ViewToolGemRodrigo
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/view_tool_gem_rodrigo/version.rb
@@ -0,0 +1,3 @@
+module ViewToolGemRodrigo
+  VERSION = "0.1.0"
+end
